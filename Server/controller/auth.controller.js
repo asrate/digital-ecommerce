604b00bed2ea8504935ab19cb0153beacf3ce9d1
asrate <asrate2121@yahoo.com>
2024-02-01 11:02:59 +0300

    complete singup and sign route and controller

--- Server/controller/auth.controller.js
+++ Server/controller/auth.controller.js
@@ -1,3 +1,33 @@
+import User from "../Model/user.model.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const test = (req, res) => {
-  res.json({ msg: "hello world" });
+  res.json({ message: "hello world" });
+};
+
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(200).json("success");
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return res.status(404).json("Invalid email or password");
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword)
+      return res.status(404).json("invalid email or password");
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res.status(200).json(token);
+  } catch (error) {
+    console.error(error);
+  }
 };
--- Server/route/auth.route.js
+++ Server/route/auth.route.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { test } from "../controller/auth.controller.js";
+import { test, signup, signin } from "../controller/auth.controller.js";
 const router = express.Router();
 router.get("/test", test);
+router.post("/signup", signup);
+router.post("/signin", signin);
 export default router;
